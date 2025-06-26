b93167d69c559a845a1fe19e898cc528400b6fcd
Davy <davy.josias@gmail.com>
2025-06-25 22:57:59 -0300

    Commit ajustado consumo e dispositivo, porém não finalizado

---
+++ src/main/java/Consumo.java
@@ -0,0 +1,53 @@
+
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.Date;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author davyj
+ */
+public class Consumo {
+    private Time dataHora;
+    private float valor;
+
+    public Date getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(Time dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public Consumo(Time dataHora, float valor) {
+        this.dataHora = dataHora;
+        this.valor = valor;
+    }
+
+    public Consumo(float valor) {
+        this.valor = valor;
+        this.dataHora = Time.valueOf(LocalTime.now()) ;//Não descobri como pegar a data e a hora atual.
+    }
+
+    public Consumo() {}
+    
+    
+    
+    
+    
+    
+    
+}
--- src/main/java/com/mycompany/projetofinal/Dispositivo.java
+++ src/main/java/com/mycompany/projetofinal/Dispositivo.java
@@ -4,7 +4,73 @@
  */
 package com.mycompany.projetofinal;
 
+import java.util.List;
+
 
 public class Dispositivo {
+    private int id;
+    private String nome;
+    private String modelo;
+    private String tipo;
+    private String moradia;
+    private List<Consumo> historicoConsumo;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getMoradia() {
+        return moradia;
+    }
+    public void setMoradia(String moradia) {
+        this.moradia = moradia;
+    }
+
+    public List<Consumo> getHistoricoConsumo() {
+        return historicoConsumo;
+    }
+    public void setHistoricoConsumo(List<Consumo> historicoConsumo) {
+        this.historicoConsumo = historicoConsumo;
+    }
+
+    public Dispositivo(int id, String nome, String modelo, String tipo, String moradia, List<Consumo> historicoConsumo) {
+        this.id = id;
+        this.nome = nome;
+        this.modelo = modelo;
+        this.tipo = tipo;
+        this.moradia = moradia;
+        this.historicoConsumo = historicoConsumo;
+    }
+    
+    public Dispositivo(){}
+    
+    public registrarConsumo(Consumo consumo){
+        
+    }
+    
     
 }
