a89f089c7d2d60470e56aedf973b62c0a3a3956a
SamuelDeMarcoDev <samuel.demarco.dev@gmail.com>
2025-06-25 21:31:30 -0300

    Inserindo estrutura classe Consumo

--- src/main/java/com/mycompany/projetofinal/Consumo.java
+++ src/main/java/com/mycompany/projetofinal/Consumo.java
@@ -3,7 +3,31 @@ package com.mycompany.projetofinal;
 import java.util.Date;
 
 public class Consumo {
+    //ATRIBUTOS
     private Date dataHora;
-    
-    
+    private float valor;
+
+    //CONSTRUTORES
+    public Consumo(Date dataHora, float valor) {
+        this.dataHora = dataHora;
+        this.valor = valor;
+    }
+    public Consumo(){}
+
+    //GETTERS E SETTERS
+    public Date getDataHora() {
+        return dataHora;
+    }
+    public void setDataHora(Date dataHora) {
+        this.dataHora = dataHora;
+    }
+    public float getValor() {
+        return valor;
+    }
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    //MÉTODOS
+
 }
